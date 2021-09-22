b38fd06f977c4dbb90c5c1c39fdf83540cd8775d
Rizqi Danisya Krisna <83813922+1910631170127RizqiDanisyaKrisna@users.noreply.github.com>
2021-09-22 20:03:04 +0700

    Tugas Programming Chapter 5
    Tugas Programming Chapter 5
    Angry Bird
    Rizqi Danisya Krisna
    ID : 284

---
+++ Angry Bird Game/Assets/Script/ExplodeBird.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeBird : Bird
+{
+    public float areaLedakan;
+    public float kekuatanLedakan;
+    public LayerMask triggerLedakan;
+    public GameObject effect;
+    public GameObject BombBird;
+
+    public override void OnCollisionEnter2D(Collision2D col)
+    {
+       StartCoroutine(explode());
+    }
+    IEnumerator explode()
+    {
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, areaLedakan, triggerLedakan);
+    
+        foreach(Collider2D obj in objects)
+        {
+            Vector2 direction = obj.transform.position - transform.position;
+
+            obj.GetComponent<Rigidbody2D>().AddForce(direction * kekuatanLedakan);
+        }
+        Instantiate(effect, BombBird.transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(0.001f);
+        Destroy(gameObject);
+    }    
+}
